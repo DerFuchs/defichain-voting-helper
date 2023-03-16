27556878a3ecafbd181d8240d0cb0ee201f3101c
Michael Fuchs <michael@derfuchs.net>
2023-03-16 22:35:24 +0100

    [Improvement] Added helper text for Windows users to help them find their defi.con file

--- src/components/SetupWalletConnection.vue
+++ src/components/SetupWalletConnection.vue
@@ -1,22 +1,36 @@
 <template>
-	<expand-item name="setup" headline="Setup Wallet Connection">
+	<expand-item
+		name="setup"
+		headline="Set Up Wallet Connection"
+		:force-expand="!node.hasCredentials"
+	>
 		<p>
 			This app is not able to read from your computer's local file system for
 			security reasons. Therefore, you have to fill in the access key for the
 			DeFiChain Node, running alongside your DeFiChain Full Wallet. No need for
 			panic! It's easy!
 		</p>
-		<p class="q-mb-xs">Open Finder and navigate to the file:</p>
-		<i>~/Library/Application Support/Defi/.defi/defi.conf</i>
-		<p class="q-mt-xs">
-			Alternatively, open Finder, have a look at the menu bar and choose "Go To"
-			=> "Go To Folder" and paste in the path from above.
-		</p>
-		<p>
-			Open the file <i>defi.conf</i> with a text editor like "Text Edit"
-			(right-click => open => "TextEdit.app")
-		</p>
-		<p>Find these two values and put their values in these text fields:</p>
+		<div v-if="platform.is.windows">
+			<p class="q-mb-xs">Open Explorer and navigate to the file:</p>
+			<i>C:\Users\username\.defi\defi.conf</i>
+			<p>
+				Open the file <i>defi.conf</i> with a text editor like "Notepad"
+				(right-click => open with => "Nodepad")
+			</p>
+		</div>
+		<div v-if="platform.is.mac">
+			<p class="q-mb-xs">Open Finder and navigate to the file:</p>
+			<i>~/Library/Application Support/Defi/.defi/defi.conf</i>
+			<p class="q-mt-xs">
+				Alternatively, open Finder, have a look at the menu bar and choose "Go
+				To" => "Go To Folder" and paste in the path from above.
+			</p>
+			<p>
+				Open the file <i>defi.conf</i> with a text editor like "Text Edit"
+				(right-click => open => "TextEdit.app")
+			</p>
+		</div>
+		<p>Find these two keys and put their values in these text fields:</p>
 		<q-input v-model="rpcauth" outlined type="text" label="rpcauth" />
 		<q-input
 			v-model="rpcport"
@@ -43,6 +57,8 @@
 <script>
 import { defineComponent, onBeforeMount, ref } from "vue";
 
+import { useQuasar } from "quasar";
+
 import { useNodeStore } from "stores/node";
 import { useChainStore } from "stores/chain";
 import { useMasternodesStore } from "stores/masternodes";
@@ -54,6 +70,8 @@ export default defineComponent({
 	name: "SetupWalletConnection",
 	components: { ExpandItem },
 	setup() {
+		const $q = useQuasar();
+
 		const node = useNodeStore();
 		const chain = useChainStore();
 		const masternodes = useMasternodesStore();
@@ -81,6 +99,7 @@ export default defineComponent({
 		}
 
 		return {
+			platform: $q.platform,
 			node,
 			rpcauth,
 			rpcport,
