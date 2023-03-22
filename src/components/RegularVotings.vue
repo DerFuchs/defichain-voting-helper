97f3df25d8350c5a3eb43fc766c401b16d882539
Michael Fuchs <michael@derfuchs.net>
2023-03-22 01:35:02 +0100

    [improved] Using badges for displaying the number of currently running votings

--- src/components/RegularVotings.vue
+++ src/components/RegularVotings.vue
@@ -1,6 +1,9 @@
 <template>
 	<div class="text-h6 q-pa-sm q-pl-md">
-		Currently Running Regular Votings ({{ proposals.regular.length }})
+		Currently Running Regular Votings
+		<q-badge class="jelly-gradient" align="top">
+			{{ proposals.regular.length }}
+		</q-badge>
 	</div>
 	<q-separator class="jelly-gradient q-mb-md" />
 	<div class="row q-mb-md">
