b626826ab83dca128ce86d8e7cbbe572e2285737
Michael Fuchs <michael@derfuchs.net>
2023-03-22 01:50:55 +0100

    Version bump 1 => 2

--- src/stores/basics.js
+++ src/stores/basics.js
@@ -5,7 +5,7 @@ import { useQuasar } from "quasar";
 export const useBasicsStore = defineStore(
   "basics",
   () => {
-    const appVersion = 1;
+    const appVersion = 2;
     const darkMode = computed(() => useQuasar().dark.isActive);
     const formatting = {
       currency: {
